228762d19041086b3c8ec2c62dd53de9f21ce989
Nathan Burgess <nburgess96@gmail.com>
2023-07-15 19:32:23 -0400

    7/15 Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { ChakraProvider } from "@chakra-ui/react";
 import './App.css';
+import Navbottom from './components/Navbottom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Specials from './components/Specials';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <ChakraProvider>
+      <main>
+        <Header />
+      </main>
+    </ChakraProvider>
   );
 }
 
 export default App;
+
+
+
+/* <Header />
+<Specials />
+<Testimonials />
+<About />
+<Navbottom /> */
